eb696fb719f174dea74e5223c307a6ddd27f0359
Maciej Raszplewicz <maciej.raszplewicz@gmail.com>
2020-09-22 14:06:12 +0200

    Terraform S3 bucket encryption

--- src/integTest/groovy/io/devopsbox/infrastructure/test/s3/S3TerraformModuleTest.groovy
+++ src/integTest/groovy/io/devopsbox/infrastructure/test/s3/S3TerraformModuleTest.groovy
@@ -3,7 +3,9 @@ package io.devopsbox.infrastructure.test.s3
 import io.devopsbox.infrastructure.test.common.terraform.TerraformIntegrationTest
 import software.amazon.awssdk.regions.Region
 import software.amazon.awssdk.services.s3.S3Client
+import software.amazon.awssdk.services.s3.model.GetBucketEncryptionRequest
 import software.amazon.awssdk.services.s3.model.ListBucketsRequest
+import software.amazon.awssdk.services.s3.model.ServerSideEncryption
 import spock.lang.Shared
 
 class S3TerraformModuleTest extends TerraformIntegrationTest {
@@ -32,6 +34,7 @@ class S3TerraformModuleTest extends TerraformIntegrationTest {
         then:
         def bucketName = "acme-" + environmentName() + "-tfzbigniew-pictures"
         checkBucketExists(bucketName)
+        isBucketEncrypted(bucketName)
 
         cleanup:
         destroyTerraformModule("terraform/s3", constructProps)
@@ -42,5 +45,15 @@ class S3TerraformModuleTest extends TerraformIntegrationTest {
                 .build())
 
         assert listBucketsResponse.buckets().any { name.equals(it.name()) }
+    }
+
+    void isBucketEncrypted(String bucketName) {
+        def getBucketEncryptionResponse = s3Client.getBucketEncryption(GetBucketEncryptionRequest.builder()
+                .bucket(bucketName)
+                .build())
+
+        assert getBucketEncryptionResponse.serverSideEncryptionConfiguration().rules().any {
+            it.applyServerSideEncryptionByDefault().sseAlgorithm() == ServerSideEncryption.AWS_KMS
+        }
     }
 }
--- terraform/s3/main.tf
+++ terraform/s3/main.tf
@@ -8,4 +8,11 @@ terraform {
 
 resource "aws_s3_bucket" "bucket" {
   bucket = "${var.companyName}-${var.envName}-${var.appName}-${var.bucketPurpose}"
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "aws:kms"
+      }
+    }
+  }
 }
