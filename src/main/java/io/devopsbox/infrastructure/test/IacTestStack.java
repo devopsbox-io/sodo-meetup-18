c9de7c1725f61035b88d103f09cfe30704e131bd
Maciej Raszplewicz <maciej.raszplewicz@gmail.com>
2020-09-23 09:03:01 +0200

    CDK application

--- src/main/java/io/devopsbox/infrastructure/test/IacTestStack.java
+++ src/main/java/io/devopsbox/infrastructure/test/IacTestStack.java
@@ -1,5 +1,7 @@
 package io.devopsbox.infrastructure.test;
 
+import io.devopsbox.infrastructure.test.s3.S3Construct;
+import io.devopsbox.infrastructure.test.s3.S3ConstructProps;
 import software.amazon.awscdk.core.Construct;
 import software.amazon.awscdk.core.Stack;
 import software.amazon.awscdk.core.StackProps;
@@ -12,6 +14,11 @@ public class IacTestStack extends Stack {
     public IacTestStack(final Construct scope, final String id, final StackProps props) {
         super(scope, id, props);
 
-
+        S3Construct construct = new S3Construct(this, "pictures", new S3ConstructProps(
+                "acme",
+                "dev",
+                "zbigniew",
+                "pictures"
+        ));
     }
 }
